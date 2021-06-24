7a94d51c77d4b5396a2219944ea4805207b7cf8d
Daniil Borisovskii <vender98@mail.ru>
2021-06-25 02:01:22 +0300

    Refactor the "search()" method of "Api" to the "searchCities()"

--- app/src/main/java/com/vender98/aviasearch/data/network/Api.kt
+++ app/src/main/java/com/vender98/aviasearch/data/network/Api.kt
@@ -1,9 +1,14 @@
 package com.vender98.aviasearch.data.network
 
+import com.vender98.aviasearch.domain.CitySearchResponse
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface Api {
 
     @GET("/autocomplete")
-    suspend fun search()
+    suspend fun searchCities(
+        @Query(value = "term") term: String,
+        @Query(value = "lang") lang: String
+    ): CitySearchResponse
 }
