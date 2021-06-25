9769924a44252a80f4ddcdab42e77f5d0d7dfafd
Daniil Borisovskii <vender98@mail.ru>
2021-06-25 22:28:21 +0300

    Add "MapView" on "Search tickets" screen

--- app/src/main/java/com/vender98/aviasearch/ui/screens/searchtickets/SearchTicketsFragment.kt
+++ app/src/main/java/com/vender98/aviasearch/ui/screens/searchtickets/SearchTicketsFragment.kt
@@ -1,10 +1,30 @@
 package com.vender98.aviasearch.ui.screens.searchtickets
 
+import android.os.Bundle
+import android.view.View
 import androidx.core.os.bundleOf
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
 import com.vender98.aviasearch.R
+import com.vender98.aviasearch.databinding.FragmentSearchTicketsBinding
 import com.vender98.aviasearch.ui.base.BaseFragment
 
-class SearchTicketsFragment : BaseFragment(R.layout.activity_main) {
+class SearchTicketsFragment : BaseFragment(R.layout.fragment_search_tickets), OnMapReadyCallback {
+
+    private val binding: FragmentSearchTicketsBinding by viewBinding()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding) {
+            mapView.onCreate(savedInstanceState)
+            mapView.getMapAsync(this@SearchTicketsFragment)
+        }
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        // TODO: work with map
+    }
 
     companion object {
         private const val ROUTE_ARG = "ROUTE_ARG"
