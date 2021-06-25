56b25d55e8d304cd57c12d946ac4faa2ff61fc50
Daniil Borisovskii <vender98@mail.ru>
2021-06-25 21:06:21 +0300

    Check Google Play services availability on application start

--- app/src/main/java/com/vender98/aviasearch/presentation/MainViewModel.kt
+++ app/src/main/java/com/vender98/aviasearch/presentation/MainViewModel.kt
@@ -1,14 +1,33 @@
 package com.vender98.aviasearch.presentation
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
 import com.vender98.aviasearch.presentation.common.ErrorBus
+import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import javax.inject.Inject
 
 class MainViewModel @Inject constructor(
-    errorBus: ErrorBus
-) : ViewModel() {
+    application: Application,
+    errorBus: ErrorBus,
+) : AndroidViewModel(application) {
 
     val errorsFlow: Flow<String> = errorBus.observe()
+    private val _googlePlayServicesInaccessibilityEventsFlow = MutableSharedFlow<Unit>(
+        extraBufferCapacity = 1,
+        onBufferOverflow = BufferOverflow.DROP_OLDEST
+    )
+    val googlePlayServicesInaccessibilityEventsFlow = _googlePlayServicesInaccessibilityEventsFlow.asSharedFlow()
+
+    fun checkGooglePlayServicesAvailability() {
+        val googleApiAvailability = GoogleApiAvailability.getInstance()
+        if (googleApiAvailability.isGooglePlayServicesAvailable(getApplication()) != ConnectionResult.SUCCESS) {
+            _googlePlayServicesInaccessibilityEventsFlow.tryEmit(Unit)
+        }
+    }
 
 }
--- app/src/main/java/com/vender98/aviasearch/ui/MainActivity.kt
+++ app/src/main/java/com/vender98/aviasearch/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.vender98.aviasearch.ui
 
+import android.app.Dialog
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -9,7 +10,8 @@ import by.kirich1409.viewbindingdelegate.viewBinding
 import com.google.android.material.snackbar.Snackbar
 import com.vender98.aviasearch.R
 import com.vender98.aviasearch.databinding.ActivityMainBinding
-import com.vender98.aviasearch.di.ToothpickViewModelFactory
+import com.vender98.aviasearch.di.ToothpickAndroidViewModelFactory
+import com.vender98.aviasearch.extensions.showAlertDialog
 import com.vender98.aviasearch.presentation.MainViewModel
 import com.vender98.aviasearch.ui.screens.choosedestination.ChooseDestinationFragment
 import kotlinx.coroutines.flow.collect
@@ -21,6 +23,8 @@ class MainActivity : AppCompatActivity() {
         get() = supportFragmentManager.findFragmentById(R.id.fragment_container)
     private val binding: ActivityMainBinding by viewBinding(R.id.fragment_container)
     private lateinit var viewModel: MainViewModel
+
+    private var alertDialog: Dialog? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,14 +38,30 @@ class MainActivity : AppCompatActivity() {
         }
 
         viewModel =
-            ViewModelProvider(this, ToothpickViewModelFactory()).get(MainViewModel::class.java)
+            ViewModelProvider(this, ToothpickAndroidViewModelFactory(application)).get(MainViewModel::class.java)
         observeViewModel()
+        viewModel.checkGooglePlayServicesAvailability()
     }
 
     private fun observeViewModel() {
         lifecycleScope.launch {
             viewModel.errorsFlow.collect { showError(it) }
         }
+        lifecycleScope.launch {
+            viewModel.googlePlayServicesInaccessibilityEventsFlow.collect {
+                showAlertDialog(
+                    message = getString(R.string.main_google_play_services_inaccessibility_message),
+                    onPositiveCallback = this@MainActivity::finish,
+                    isCancelable = false
+                )
+                    .also { alertDialog = it }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        alertDialog?.dismiss()
+        super.onDestroy()
     }
 
     private fun showError(message: String) {
