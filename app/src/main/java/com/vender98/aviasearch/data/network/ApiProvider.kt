84c53d2c75243d82542d796a0db3227664970356
Daniil Borisovskii <vender98@mail.ru>
2021-06-26 04:25:57 +0300

    Correct the converter factory for Retrofit

--- app/src/main/java/com/vender98/aviasearch/data/network/ApiProvider.kt
+++ app/src/main/java/com/vender98/aviasearch/data/network/ApiProvider.kt
@@ -1,6 +1,7 @@
 package com.vender98.aviasearch.data.network
 
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.squareup.moshi.Moshi
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -8,12 +9,13 @@ import javax.inject.Inject
 import javax.inject.Provider
 
 class ApiProvider @Inject constructor(
-    private val okHttpClient: OkHttpClient
+    private val okHttpClient: OkHttpClient,
+    private val moshi: Moshi
 ) : Provider<Api> {
 
     override fun get(): Api =
         Retrofit.Builder()
-            .addConverterFactory(MoshiConverterFactory.create())
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
             .addCallAdapterFactory(CoroutineCallAdapterFactory())
             .client(okHttpClient)
             .baseUrl("https://yasen.hotellook.com")
