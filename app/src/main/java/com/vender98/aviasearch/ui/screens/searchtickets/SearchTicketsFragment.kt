6a2f90344f6d7c0b86b70da93b3fc1cda51e17e1
Daniil Borisovskii <vender98@mail.ru>
2021-06-26 14:18:27 +0300

    Add logic to show route endpoints on "Search tickets" screen

--- app/src/main/java/com/vender98/aviasearch/ui/screens/searchtickets/SearchTicketsFragment.kt
+++ app/src/main/java/com/vender98/aviasearch/ui/screens/searchtickets/SearchTicketsFragment.kt
@@ -1,33 +1,136 @@
 package com.vender98.aviasearch.ui.screens.searchtickets
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.View
 import androidx.core.os.bundleOf
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.maps.android.ktx.awaitMap
 import com.vender98.aviasearch.R
 import com.vender98.aviasearch.databinding.FragmentSearchTicketsBinding
+import com.vender98.aviasearch.di.ToothpickViewModelFactory
+import com.vender98.aviasearch.presentation.screens.searchtickets.SearchTicketsViewModel
 import com.vender98.aviasearch.ui.base.BaseFragment
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
-class SearchTicketsFragment : BaseFragment(R.layout.fragment_search_tickets), OnMapReadyCallback {
+class SearchTicketsFragment : BaseFragment(R.layout.fragment_search_tickets) {
 
     private val binding: FragmentSearchTicketsBinding by viewBinding()
+    private val viewModel: SearchTicketsViewModel by viewModels {
+        ToothpickViewModelFactory(bindDependencies = {
+            bind(Route::class.java).toInstance(requireArguments()[ROUTE_ARG] as Route)
+        })
+    }
+
+    private var map: GoogleMap? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        with(binding) {
-            mapView.onCreate(savedInstanceState)
-            mapView.getMapAsync(this@SearchTicketsFragment)
+        with(binding.mapView) {
+            onCreate(savedInstanceState?.getBundle(MAPVIEW_BUNDLE_KEY))
+            lifecycleScope.launch {
+                map = awaitMap().also {
+                    it.setOnMarkerClickListener { true }
+                }
+                viewModel.onMapReady()
+            }
         }
     }
 
-    override fun onMapReady(map: GoogleMap) {
-        // TODO: work with map
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY) ?: Bundle().also {
+            outState.putBundle(MAPVIEW_BUNDLE_KEY, it)
+        }
+        binding.mapView.onSaveInstanceState(mapViewBundle)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.mapView.onStart()
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        lifecycleScope.launchWhenStartedUntilStop {
+            viewModel.routeFlow.collect { showRouteEndpoints(it) }
+        }
+    }
+
+    private fun showRouteEndpoints(route: Route) {
+        val map = this.map ?: return
+
+        val departureMarker =
+            MarkerOptions()
+                .position(route.departureCity.location)
+                .icon(
+                    BitmapDescriptorFactory.fromBitmap(
+                        CityMarkerBitmap(requireContext(), route.departureCity.name)
+                    )
+                )
+        val destinationMarker =
+            MarkerOptions()
+                .position(route.destinationCity.location)
+                .icon(
+                    BitmapDescriptorFactory.fromBitmap(
+                        CityMarkerBitmap(requireContext(), route.destinationCity.name)
+                    )
+                )
+        map.addMarker(departureMarker)
+        map.addMarker(destinationMarker)
+
+        val bounds = LatLngBounds.Builder()
+            .include(departureMarker.position)
+            .include(destinationMarker.position)
+            .build()
+        val padding =
+            if (resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
+                (resources.displayMetrics.widthPixels * 0.2).toInt()
+            } else {
+                (resources.displayMetrics.heightPixels * 0.2).toInt()
+            }
+
+        val boundsUpdate = CameraUpdateFactory.newLatLngBounds(bounds, padding)
+        map.moveCamera(boundsUpdate)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.mapView.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView.onLowMemory()
+    }
+
+    override fun onDestroyView() {
+        map = null
+        binding.mapView.onDestroy()
+        super.onDestroyView()
     }
 
     companion object {
         private const val ROUTE_ARG = "ROUTE_ARG"
+        private const val MAPVIEW_BUNDLE_KEY = "MAPVIEW_BUNDLE_KEY"
 
         fun newInstance(route: Route) = SearchTicketsFragment().apply {
             arguments = bundleOf(ROUTE_ARG to route)
